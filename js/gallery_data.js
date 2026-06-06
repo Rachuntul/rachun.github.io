dea4afaba81db2af9efbf856ae57fec640bc0500
rachuntul <dionpujiramdani@gmail.com>
2026-06-07 00:32:12 +0700

    Auto-update: 2026-06-07 00:32:12

--- js/gallery_data.js
+++ js/gallery_data.js
@@ -1,13 +1,13 @@
 // Auto-generated gallery data
-window.galleryPhotos = ["img/gallery/DSCF5053.webp", "img/gallery/DSCF2549.webp", "img/gallery/DSCF2577.webp", "img/gallery/DSCF2559.webp", "img/gallery/DSCF2555.webp", "img/gallery/DSCF5034.webp", "img/gallery/DSCF5362.webp", "img/gallery/DSCF5862.webp", "img/gallery/DSCF5760.webp", "img/gallery/DSCF2667.webp", "img/gallery/DSCF5999.webp", "img/gallery/DSCF5375.webp", "img/gallery/DSCF2720.webp", "img/gallery/DSCF5351.webp", "img/gallery/DSCF2562.webp", "img/gallery/DSCF5926.webp", "img/gallery/DSCF5842.webp", "img/gallery/DSCF5388.webp", "img/gallery/DSCF5026.webp", "img/gallery/DSCF5974.webp", "img/gallery/DSCF5924.webp", "img/gallery/DSCF5041.webp", "img/gallery/DSCF2662.webp"];
-window.flyerPhotos = [];
+window.galleryPhotos = ["img/gallery/DSCF5026.webp", "img/gallery/DSCF2549.webp", "img/gallery/DSCF5974.webp", "img/gallery/DSCF2577.webp", "img/gallery/DSCF2559.webp", "img/gallery/DSCF2555.webp", "img/gallery/DSCF5842.webp", "img/gallery/DSCF5034.webp", "img/gallery/DSCF5999.webp", "img/gallery/DSCF5862.webp", "img/gallery/DSCF5053.webp", "img/gallery/DSCF2667.webp", "img/gallery/DSCF5362.webp", "img/gallery/DSCF2720.webp", "img/gallery/DSCF5351.webp", "img/gallery/DSCF2562.webp", "img/gallery/DSCF5041.webp", "img/gallery/DSCF5926.webp", "img/gallery/DSCF5924.webp", "img/gallery/DSCF5388.webp", "img/gallery/DSCF2662.webp", "img/gallery/DSCF5375.webp", "img/gallery/DSCF5760.webp"];
+window.flyerPhotos = ["img/flyer/Damn1.webp"];
 window.galleryStatus = {
   isEmpty: false,
   count: 23,
-  lastUpdate: '2026-06-07 00:25:52'
+  lastUpdate: '2026-06-07 00:32:10'
 };
 window.flyerStatus = {
-  isEmpty: true,
-  count: 0,
-  lastUpdate: '2026-06-07 00:25:52'
+  isEmpty: false,
+  count: 1,
+  lastUpdate: '2026-06-07 00:32:10'
 };
