bf0d04c0f462f90de587a32b620edf0b7b0b189f
rachuntul <dionpujiramdani@gmail.com>
2026-09-16 23:37:24 +0700

    Update gallery and website

--- js/gallery_data.js
+++ js/gallery_data.js
@@ -1,13 +1,13 @@
 // Auto-generated gallery data
-window.galleryPhotos = ["img/gallery/2026-03-25_001.webp", "img/gallery/2026-05-21_001.webp", "img/gallery/2026-05-21_002.webp", "img/gallery/2026-05-21_003.webp", "img/gallery/2026-05-21_004.webp", "img/gallery/2026-05-21_005.webp", "img/gallery/2026-05-21_006.webp", "img/gallery/2026-05-21_007.webp", "img/gallery/2026-05-21_008.webp", "img/gallery/2026-06-07_001.webp", "img/gallery/2026-06-07_002.webp", "img/gallery/2026-06-07_003.webp", "img/gallery/2026-06-07_004.webp", "img/gallery/2026-06-07_005.webp", "img/gallery/2026-06-07_006.webp", "img/gallery/2026-06-07_007.webp", "img/gallery/2026-06-07_008.webp", "img/gallery/2026-06-07_009.webp", "img/gallery/2026-06-07_010.webp", "img/gallery/2026-06-07_011.webp", "img/gallery/2026-06-07_012.webp", "img/gallery/2026-06-07_013.webp", "img/gallery/2026-06-07_014.webp", "img/gallery/2026-06-07_015.webp", "img/gallery/2026-06-10_001.webp", "img/gallery/2026-06-10_002.webp", "img/gallery/2026-06-10_003.webp", "img/gallery/2026-06-10_004.webp", "img/gallery/2026-06-10_005.webp", "img/gallery/2026-06-14_001.webp", "img/gallery/2026-06-14_002.webp", "img/gallery/2026-06-14_003.webp", "img/gallery/2026-06-14_004.webp", "img/gallery/2026-07-29_001.webp", "img/gallery/2026-08-02_001.webp", "img/gallery/2026-08-02_002.webp"];
+window.galleryPhotos = ["img/gallery/2026-03-25_001.webp", "img/gallery/2026-05-21_001.webp", "img/gallery/2026-05-21_002.webp", "img/gallery/2026-05-21_003.webp", "img/gallery/2026-05-21_004.webp", "img/gallery/2026-05-21_005.webp", "img/gallery/2026-05-21_006.webp", "img/gallery/2026-05-21_007.webp", "img/gallery/2026-05-21_008.webp", "img/gallery/2026-06-07_001.webp", "img/gallery/2026-06-07_002.webp", "img/gallery/2026-06-07_003.webp", "img/gallery/2026-06-07_004.webp", "img/gallery/2026-06-07_005.webp", "img/gallery/2026-06-07_006.webp", "img/gallery/2026-06-07_007.webp", "img/gallery/2026-06-07_008.webp", "img/gallery/2026-06-07_009.webp", "img/gallery/2026-06-07_010.webp", "img/gallery/2026-06-07_011.webp", "img/gallery/2026-06-07_012.webp", "img/gallery/2026-06-07_013.webp", "img/gallery/2026-06-07_014.webp", "img/gallery/2026-06-07_015.webp", "img/gallery/2026-06-10_001.webp", "img/gallery/2026-06-10_002.webp", "img/gallery/2026-06-10_003.webp", "img/gallery/2026-06-10_004.webp", "img/gallery/2026-06-10_005.webp", "img/gallery/2026-06-14_001.webp", "img/gallery/2026-06-14_002.webp", "img/gallery/2026-06-14_003.webp", "img/gallery/2026-06-14_004.webp", "img/gallery/2026-09-06_001.webp", "img/gallery/2026-09-06_002.webp", "img/gallery/2026-09-06_003.webp", "img/gallery/2026-09-06_004.webp", "img/gallery/2026-09-06_005.webp", "img/gallery/2026-09-06_006.webp", "img/gallery/2026-09-12_001.webp", "img/gallery/2026-09-12_002.webp", "img/gallery/2026-09-12_003.webp", "img/gallery/2026-09-12_004.webp", "img/gallery/2026-09-12_005.webp", "img/gallery/2026-09-12_006.webp", "img/gallery/2026-09-12_007.webp", "img/gallery/2026-09-12_008.webp", "img/gallery/2026-09-12_009.webp", "img/gallery/2026-09-12_010.webp", "img/gallery/2026-09-12_011.webp", "img/gallery/2026-09-12_012.webp"];
 window.flyerPhotos = ["img/flyer/2026-06-07_001.webp", "img/flyer/2026-06-18_001.webp", "img/flyer/2026-06-18_002.webp"];
 window.galleryStatus = {
   isEmpty: false,
-  count: 36,
-  lastUpdate: '2026-08-03 01:01:59'
+  count: 51,
+  lastUpdate: '2026-09-16 23:31:51'
 };
 window.flyerStatus = {
   isEmpty: false,
   count: 3,
-  lastUpdate: '2026-08-03 01:01:59'
+  lastUpdate: '2026-09-16 23:31:51'
 };
